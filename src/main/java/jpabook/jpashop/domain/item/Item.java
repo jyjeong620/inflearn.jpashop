612fabccd6fb6357ca5d68dd71c6ff6f00dafca2
정준영 <jeongjy@mrblue.com>
2023-07-19 16:18:43 +0900

    feat: 상품 엔티티 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype") // 상속 관계 매핑을 위한 구분 컬럼을 지정
@@ -25,4 +27,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items") // 다대다 관계 매핑
     private List<Category> categories = new ArrayList<>();
+
+    //===비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        stockQuantity = restStock;
+    }
 }
