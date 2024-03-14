6298843dd9352e082788a565a916635887c024d6
정준영 <jeongjy@mrblue.com>
2024-03-14 09:28:25 +0900

    feat: 엔티티 직접 노출 예제

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,26 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
+    /**
+     * hibernate에서 lazy loading을 강제로 호출하기 위한 설정
+     * <p>
+     * 실제 환경에서는 Entity자체를 응답하는것을 지양하기때문에 사용할 필요가 없다.
+     */
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
